4c91915540d0879507ee8aa13e577f17ba007a06
mahmoudramz <52760874+mahmoudramz@users.noreply.github.com>
2019-08-13 13:46:09 -0400

    Add files via upload

---
+++ assets/myProfile.ts
@@ -0,0 +1,6 @@
+import {shikhhus} from '../app/shikhhus';
+
+export const MYPROFILE : shikhhus= {
+    studentNumber :'991503676', name : 'Mahmoud Shikh-hussin',
+    loginName :'Shikhhus', campus :'Davis', assignmentTitile:'Assignment4'
+};
